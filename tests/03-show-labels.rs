5f7d89fdd5257f1076590ad36cda7eff33993311
Andinus <andinus@nand.sh>
2023-12-04 21:10:54 +0530

    Add show_labels test with alternate delimiters

--- tests/03-show-labels.rs
+++ tests/03-show-labels.rs
@@ -30,3 +30,33 @@ fn render_with_show_labels() -> Result<(), String> {
     assert_eq!(nest.render(&page)?, nest_no_labels.render(&page_output)?,);
     Ok(())
 }
+
+#[test]
+fn render_with_show_labels_alt_delimiters() -> Result<(), String> {
+    let nest = TemplateNest {
+        directory: "templates".into(),
+        show_labels: true,
+        comment_delimiters: (&"<!--!", &"!-->"),
+        ..Default::default()
+    };
+    let page = filling!(
+        "TEMPLATE": "00-simple-page",
+        "variable": "Simple Variable",
+        "simple_component":  {
+            "TEMPLATE":"01-simple-component",
+            "variable": "Simple Variable in Simple Component"
+        }
+    );
+
+    let nest_no_labels = TemplateNest {
+        directory: "templates".into(),
+        show_labels: false,
+        ..Default::default()
+    };
+    let page_output = filling!(
+        "TEMPLATE": "output/05-simple-page-with-labels-alt-delims",
+    );
+
+    assert_eq!(nest.render(&page)?, nest_no_labels.render(&page_output)?,);
+    Ok(())
+}
